8d1f960591bba084819c3203d376196fcbdf5690
test <mail>
2023-02-17 17:28:14 +0900

    initialize

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RequestMapping("/")
+//@RestController
+@Controller
+public class TestController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+
+    public String hello() {
+        return "tester";
+    }
+}
