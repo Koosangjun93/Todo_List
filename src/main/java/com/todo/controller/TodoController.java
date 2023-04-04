84d182c71369054ccd751f2e42f7311b35d5d626
smpark02 <smpark02@thirautech.com>
2023-04-04 11:00:59 +0900

    log test

--- src/main/java/com/todo/controller/TodoController.java
+++ src/main/java/com/todo/controller/TodoController.java
@@ -3,6 +3,9 @@ package com.todo.controller;
 import com.todo.dao.Todo;
 import com.todo.response.BasicResponse;
 import com.todo.service.TodoService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,12 +16,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/todos")
 public class TodoController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     @Autowired
     private TodoService service;
 
     @GetMapping
     public BasicResponse getTodoList() {
         BasicResponse basicResponse = new BasicResponse();
+        logger.info("INFO Level 테스트");
         List<Todo> todoList = service.getToDoList();
 
         if(todoList.isEmpty()) {
@@ -41,7 +46,15 @@ public class TodoController {
     @PostMapping
     public int createTodoList(@RequestBody Todo todo) {
         BasicResponse basicResponse = new BasicResponse();
-
+        int res = service.createToDoListService(todo);
+        if(res > 0) {
+            basicResponse = BasicResponse.builder()
+                    .code(HttpStatus.OK.value())
+                    .httpStatus(HttpStatus.OK)
+                    .message("Todo 목록을 추가했습니다.")
+                    .result(Collections.singletonList(res))
+                    .build();
+        }
         return service.createToDoListService(todo);
     }
 
